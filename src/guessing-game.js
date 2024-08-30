d405933ff8c476dc88bb54bc1a932d574a9d97a7
Svetlana Evdokimova <7757331@gmail.com>
2024-08-30 17:25:23 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.average;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.average = Math.round((this.max + this.min) / 2);
+        return this.average;
     }
 
     lower() {
-
+        this.max = this.average;
     }
 
     greater() {
-
+        this.min = this.average;
     }
 }
 
